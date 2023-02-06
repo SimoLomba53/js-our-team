0e701034c1e90ef591c3511d7de91b4e725d267f
SimoLomba53 <simolomba253@gmail.com>
2023-02-06 16:06:52 +0100

    informazioni stampate sul dom

--- js/main.js
+++ js/main.js
@@ -44,3 +44,43 @@ const team = [
 //STAMPO SU CONSOLE LE INFROMAZIONI
 console.log(team);
 //STAMPO LE INFORMAZIONI SU DOM SOTTOFORMA DI STRINGHE
+const containerCards = document.getElementById("container")
+const wayneCard = document.getElementById("wayne");
+const angelaCarollCard = document.getElementById("angelacaroll");
+const walterCard = document.getElementById("walter");
+const angelaLopezCard = document.getElementById("angelalopez");
+const scottCard = document.getElementById("scott");
+const barbaraCard = document.getElementById("barbara");
+
+containerCards.innerHTML = `
+        <div id="wayne">
+            <div>${team[0].nome}</div>
+            <div>${team[0].ruolo}</div>
+            <div>${team[0].foto}</div>
+        </div>
+        <div id="angelacaroll">
+            <div>${team[1].nome}</div>
+            <div>${team[1].ruolo}</div>
+            <div>${team[1].foto}</div>
+        </div>
+        <div id="walter">
+            <div>${team[2].nome}</div>
+            <div>${team[2].ruolo}</div>
+            <div>${team[2].foto}</div>
+        </div>
+        <div id="angelalopez">
+            <div>${team[3].nome}</div>
+            <div>${team[3].ruolo}</div>
+            <div>${team[3].foto}</div>
+        </div>
+        <div id="scott">
+            <div>${team[4].nome}</div>
+            <div>${team[4].ruolo}</div>
+            <div>${team[4].foto}</div>
+        </div>
+        <div id="barbara">
+            <div>${team[5].nome}</div>
+            <div>${team[5].ruolo}</div>
+            <div>${team[5].foto}</div>
+        </div>
+        `
