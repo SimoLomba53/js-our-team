c8106a7a3954d73a6b2228fcfd888772996988c2
SimoLomba53 <simolomba253@gmail.com>
2023-02-06 15:17:40 +0100

    arrey oggetti creato

---
+++ js/main.js
@@ -0,0 +1,45 @@
+//ARRAY SONO CARATTERIZZATI DA NOME,RUOLO,FOTO
+
+//CREO ARRAY OGGETTI CON INFORMAZIONI FORNITE
+let team = [
+    {
+        nome: "Wayne Barbnett",
+        ruolo: "Founder e CEO",
+        foto: "founder-ceo.jpg",
+    },
+
+    {
+        nome: "Angela Caroll",
+        ruolo: "Chief Editor",
+        foto: "angela-caroll-chief-editor.jpg",
+    },
+
+    {
+        nome: "Walter Gordon",
+        ruolo: "Office Manager",
+        foto: "walter-gordon-office-manager.jpg",
+    },
+
+    {
+        nome: "Angela Lopez",
+        ruolo: "Social Media Manager",
+        foto: "angela-lopez-social-media-manager.jpg",
+    },
+
+    {
+        nome: "Scott Estrada",
+        ruolo: "Developer",
+        foto: "scott-estrada-developer.jpg",
+    },
+
+    {
+        nome: "Barbara Ramos",
+        ruolo: "Graphic Designer",
+        foto: "barbara-ramos-geraphic-designer.jpg"
+    }
+
+
+
+]
+//STAMPO SU CONSOLE LE INFROMAZIONI
+//STAMPO LE INFORMAZIONI SU DOM SOTTOFORMA DI STRINGHE
